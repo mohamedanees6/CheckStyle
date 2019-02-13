015853a02db6ab975b022df55992e9f164a3b82b
Mohamed Anees <anees.m@practo.com>
2019-02-13 16:33:15 +0530

    Filtering files

--- src/main/java/format/FormatMyClass.java
+++ src/main/java/format/FormatMyClass.java
@@ -21,21 +21,27 @@ import com.google.googlejavaformat.java.FormatterException;
 
 public class FormatMyClass {
 
-
-	public static void main(String[] args) throws FormatterException, IOException {
-		Set<String> javaFilePaths = new HashSet<>();
-		try (Stream<Path> paths = Files.walk(Paths.get("/Users/mohamedanees/Documents/Work/insta-hms/instahms"))) {
-			paths.filter(
-					filePath -> Files.isRegularFile(filePath) && filePath.getFileName().toString().endsWith(".java"))
-					.forEach(javaFile -> javaFilePaths.add(javaFile.toAbsolutePath().toString()));
-		}
-		for (String javaFilePath : javaFilePaths) {
-			String fileContent = new String(Files.readAllBytes(Paths.get(javaFilePath)), StandardCharsets.UTF_8);
-			String formattedString = new Formatter().formatSource(fileContent);
-			try (PrintWriter out = new PrintWriter(javaFilePath)) {
-				out.println(formattedString);
-			}
-		}
-	}
+  public static void main(String[] args) throws FormatterException, IOException {
+    Set<String> javaFilePaths = new HashSet<>();
+    try (Stream<Path> paths = Files.walk(Paths.get(
+        "/Users/mohamedanees/Documents/Work/insta-hms/instahms/src/main/java/com/insta/hms/core/insurance"))) {
+      paths
+          .filter(filePath -> Files.isRegularFile(filePath)
+              && filePath.getFileName().toString().endsWith(".java")
+              && !filePath.getFileName().toString().toLowerCase().contains("model.java")
+              && !filePath.getFileName().toString().toLowerCase().contains("sequence.java")
+              && !filePath.getFileName().toString().toLowerCase().contains("generated"))
+          .forEach(javaFile -> javaFilePaths.add(javaFile.toAbsolutePath().toString()));
+    }
+    for (String javaFilePath : javaFilePaths) {
+      System.out.println("File path being changed is " + javaFilePath);
+      String fileContent =
+          new String(Files.readAllBytes(Paths.get(javaFilePath)), StandardCharsets.UTF_8);
+      String formattedString = new Formatter().formatSource(fileContent);
+      try (PrintWriter out = new PrintWriter(javaFilePath)) {
+        out.println(formattedString);
+      }
+    }
+  }
 
 }
